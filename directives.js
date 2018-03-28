46700c4d825524ae4b650f987d3acb696dcc07f2
Aghy <allogie@newsletter2go.com>
2018-03-28 14:22:29 +0200

    with routing

--- directives.js
+++ directives.js
@@ -1,74 +1,71 @@
-(function () {
-  var app = angular.module('directives', []);
-  app.$inject = ['$compile', '$scope'];
-  
-  app.directive("tabDir", tabDirDirective);
+var app = angular.module('directives', []);
+app.$inject = ['$compile', '$scope'];
 
-  function tabDirDirective() {
-    return {
-      restrict: 'E',
-      templateUrl: 'tab-dir.html',
-      controller: TabDirController,
-      controllerAs: 'panel'
-    }
-  }
+app.directive("tabDir", tabDirDirective);
 
-  function TabDirController() {
-    this.tab = 1;
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
-    };
+function tabDirDirective() {
+  return {
+    restrict: 'E',
+    templateUrl: 'tab-dir.html',
+    controller: TabDirController,
+    controllerAs: 'panel'
   }
-  
-  app.directive("noteDir", noteDirDirective);
+}
 
-  function noteDirDirective() {
-    return {
-      restrict: 'E',
-      templateUrl: 'note-dir.html',
-      controller: NoteDirController,
-      controllerAs: 'nc'
-    }
-  }
+function TabDirController() {
+  this.tab = 1;
+  this.selectTab = function(setTab) {
+    this.tab = setTab;
+  };
+}
 
-  function NoteDirController($compile, $scope) {
-    var vm = this;
-    var notes = [];
-    this.notes = notes;
-    
-    this.addNote = function(text) {
-      var note = {};
-      note.content = text;
-      vm.notes.push(note);
-    };
-    
-    this.removeNote = function(index) {
-      vm.notes.splice(index, 1);
-    };
-    
-    this.editNote = function(index) {
-      var listItem = '#li-' + index ;
-      $(listItem + " .hardLine").hide();
-      var editTextBox = '<input id="textEdit" type="text" ng-model="nc.notes['+index+'].content" value="{{nc.notes['+index+'].content}}" />';
-      var link = $compile(editTextBox);
-      var content = link($scope);
-      $(content).appendTo(listItem);
-      $(content).focus();
-      $('#textEdit').on('blur', this.editNoteContent);
-    };
-    
-    this.editNoteContent = function() {
-      var parent = $(this).parent();
-      var listItem = '#' + $(parent).attr('id');
-      //$(listItem + ' .content').text($('#textEdit').val());
-      $('#textEdit').remove();
-      $(listItem + " .hardLine").show();
-    };
-    
-    this.toggleClass = function(index) {
-      var listItem = "li-" + index;
-      $("#" + listItem + " .content").toggleClass("line-through");
-    };
+app.directive("noteDir", noteDirDirective);
+
+function noteDirDirective() {
+  return {
+    restrict: 'E',
+    templateUrl: 'note-dir.html',
+    controller: NoteDirController,
+    controllerAs: 'nc'
   }
-  
-}) ();
+}
+
+function NoteDirController($compile, $scope) {
+  var vm = this;
+  var notes = [];
+  this.notes = notes;
+
+  this.addNote = function(text) {
+    var note = {};
+    note.content = text;
+    vm.notes.push(note);
+  };
+
+  this.removeNote = function(index) {
+    vm.notes.splice(index, 1);
+  };
+
+  this.editNote = function(index) {
+    var listItem = '#li-' + index ;
+    $(listItem + " .hardLine").hide();
+    var editTextBox = '<input id="textEdit" type="text" ng-model="nc.notes['+index+'].content" value="{{nc.notes['+index+'].content}}" />';
+    var link = $compile(editTextBox);
+    var content = link($scope);
+    $(content).appendTo(listItem);
+    $(content).focus();
+    $('#textEdit').on('blur', this.editNoteContent);
+  };
+
+  this.editNoteContent = function() {
+    var parent = $(this).parent();
+    var listItem = '#' + $(parent).attr('id');
+    //$(listItem + ' .content').text($('#textEdit').val());
+    $('#textEdit').remove();
+    $(listItem + " .hardLine").show();
+  };
+
+  this.toggleClass = function(index) {
+    var listItem = "li-" + index;
+    $("#" + listItem + " .content").toggleClass("line-through");
+  };
+}
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 (function() {
-
-  var app = angular.module("PlunkViewer", ['directives']);
-  app.$inject = ['$http', '$scope'];
+  var app = angular.module("PlunkViewer", ['ngRoute', 'directives']);
+ // app.$inject = ['$http', '$scope'];
+  app.config(function ($routeProvider, $locationProvider) {
+            $locationProvider.html5Mode(true);
+           $routeProvider.when("/", {templateUrl: "/TabTag.html"})
+                         .when("/tag", {templateUrl: "/Tab-dir.html", controller: TabDirController, controllerAs: 'panel'})
+                         .when("/dir", {templateUrl: "/note-dir.html", controller: NoteDirController, controllerAs: 'nc'})
+                         .otherwise("/");
+      })
 }());
-
-
-
-//how to make two-way data binding between the edited note and its input
